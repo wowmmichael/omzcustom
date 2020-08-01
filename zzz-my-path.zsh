7338a57103d7b403f3c7a4ef6b4e1eb975fb0824
wowmmichael <buildoncloud@gmail.com>
2020-08-01 22:58:20 +0800

    Add vscode shortcut.

--- zzz-my-path.zsh
+++ zzz-my-path.zsh
@@ -3,5 +3,6 @@ CMDLETS=$UTILITIES/installed/cmdlets
 JETBRAIN=$UTILITIES/jetbrains
 JAVABIN=$JAVA_HOME/bin
 GOBIN="${GOPATH}/bin:${GOROOT}/bin"
+VSCODEBIN="/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
-export PATH=$JAVABIN:$CMDLETS:$GOBIN:$JETBRAIN:$PATH
+export PATH=$JAVABIN:$CMDLETS:$GOBIN:$JETBRAIN:$VSCODEBIN:$PATH
